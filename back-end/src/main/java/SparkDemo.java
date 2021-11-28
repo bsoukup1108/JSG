2c78f0998fd79c90885978a80ae284cf1942e072
bsoukup1108 <54554449+bsoukup1108@users.noreply.github.com>
2021-11-28 14:51:26 -0800

    sign-up and login

--- back-end/src/main/java/SparkDemo.java
+++ back-end/src/main/java/SparkDemo.java
@@ -1,10 +1,78 @@
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static spark.Spark.*;
 
+class UserDto{
+  public String username;
+  public String password;
+}
+
+class SignUpResponseDto{
+  public Boolean isSuccess;
+  public String message;
+
+  public SignUpResponseDto(Boolean isSuccess, String message) {
+    this.isSuccess = isSuccess;
+    this.message = message;
+  }
+}
+
+class LoginResponseDto{
+  public boolean validUsername;
+  public String message;
+
+  public LoginResponseDto(Boolean validUsername, String message){
+    this.validUsername = validUsername;
+    this.message = message;
+  }
+}
+
 public class SparkDemo {
+
+  private static Gson gson = new Gson();
+  private static List<UserDto> users = new ArrayList<>();
 
   public static void main(String[] args) {
     port(1234);
 
-    get("/hello", (req, res) -> "asd");
+    post("/api/sign-up", (req, res) ->{
+      String body = req.body();
+      System.out.println(body);
+      UserDto userDto = gson.fromJson(body, UserDto.class);
+
+      boolean isUsernameTaken = users.stream()
+              .anyMatch(u -> u.username.equals(userDto.username));
+
+      if(isUsernameTaken){
+        var signupRes = new SignUpResponseDto(false, "Username is taken");
+        return gson.toJson(signupRes);
+      }
+
+
+      users.add(userDto);
+      System.out.println("Total Users " + users.size());
+      var signupRes = new SignUpResponseDto(true, null);
+      return gson.toJson(signupRes);
+    });
+
+    post("/api/login", (req, res) -> {
+      String body = req.body();
+      System.out.println(body);
+      UserDto userDto = gson.fromJson(body, UserDto.class);
+
+      boolean notValidUsername = users.stream()
+              .anyMatch(u -> u.username.equals(userDto.username));
+
+      if(notValidUsername){
+        var loginRes = new LoginResponseDto(false,"Username is does not exist");
+        return  gson.toJson((loginRes));
+      }
+
+      var loginRes = new LoginResponseDto(true, "Login Successful!");
+      return gson.toJson(loginRes);
+    });
   }
 }
