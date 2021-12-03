fd75b669e60def59249f0493702679fa504eea81
bsoukup1108 <54554449+bsoukup1108@users.noreply.github.com>
2021-12-02 19:12:31 -0800

    transact

--- front-end/src/Transact.js
+++ front-end/src/Transact.js
@@ -3,18 +3,47 @@ import React from 'react'; // single quotes are preference
 
 //Step 2 : Create a component function that returns an element
 const Transact = () => {
+
+    const [username, Username] = React.useState('');
+    const [amount, Amount] = React.useState('');
+    const [password, Password] = React.useState('');
+
+    const handleTransaction = () => {
+
+    };
     return (
         <body>
-        <><div>
+        <div> 
+        <h1>Make a Transaction</h1>
+        
+    
+        <h2>Who Are You Sending This To?</h2>
+        
+        <div>
+        <input placeholder="username" value={username} 
+                onChange={e => Username(e.target.value)}/>
+
+        </div>
+    
+        <h2>Amount</h2>
+        <div>
+                <input placeholder="amount in dollars" type="amount" value={amount}
+                onChange={e => Amount(e.target.value)}/>
+            </div>
             
-
-        </div><div>
-                <h1>Make A Transaction</h1>
-
-            </div></>
-            </body>
-      
-    )
+            <h2>Your Password to Confirm Transaction</h2>
+            <div>
+                <input placeholder="password" type="password" value={password}
+                onChange={e => Password(e.target.value)}/>
+            </div>
+            <br></br>
+            <div>
+                <button onClick={handleTransaction} >Send Transaction</button>
+            </div>
+            </div>
+    
+    </body>
+    );
 }
    
 
