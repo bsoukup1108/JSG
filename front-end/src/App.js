1d4ebb643d99570c711f658f1e2b2db3753dc4bd
bsoukup1108 <54554449+bsoukup1108@users.noreply.github.com>
2021-12-11 02:15:19 -0800

    improved webpage

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,3 +1,4 @@
+
 import './App.css';
 
 import { Link, Switch, Route } from 'react-router-dom';
@@ -9,28 +10,35 @@ import Transact from './Transact';
 
 function App() {
   return (
-    
-    
     <body>
-
-      <div class="header">
-      <h1>JSG Payments</h1>
-      <p>A payment management site created by JSG.</p>
-    </div>
     
-    <div class="navbar">
-      <ul>
-        <a><li><Link to="/">Home</Link></li></a>
-        <a><li><Link to="/login">Login</Link></li></a>
-        <a><li><Link to="/sign-up">Sign Up</Link></li></a>
+      <div class="navbar">
+        <ul>
+        <a class="logo"><li ><Link to="/"></Link></li></a>
+       
     
         <a class="right">
-        <a><li><Link to="/makeTransaction">Make a Transaction</Link></li></a>
+         <a class="link"><li><Link to="/login">Login</Link></li></a>
+        <a class="link"><li><Link to="/sign-up">Sign Up</Link></li></a>
+        <a class="link"><li><Link to="/makeTransaction">Make a Transaction</Link></li></a>
+        
         </a>
         </ul>
       </div>
 
-      <div class="links">
+      
+          <div class="header">
+            <section id="section01" class="demo">
+              <h1>JSG PAYMENTS</h1>
+              <a href="#section02" class="scroll"><span> </span></a>
+          </section>
+          </div>
+        
+
+  
+       <div class="body" >
+<div class="links">
+  <section id="section02">
       <Switch>
           <Route path="/sign-up" component={SignUp} />
           <Route path="/login" component={Login} />
@@ -39,20 +47,14 @@ function App() {
             <Home />
           </Route>
         </Switch>
+        </section>
         </div>
-
-      ]
-
-      <footer class="footer">
-      <h3>Java Supremacy Gang</h3>
-      </footer>
-      
-       <br></br>
-        </body>
-       
+        </div>
         
-
-    
+        <footer class="footer">
+      <p class="foot">Java Supremacy Gang</p>
+      </footer>
+    </body>
   );
   }
 
--- front-end/src/Home.js
+++ front-end/src/Home.js
@@ -37,20 +37,12 @@ window.addEventListener('load', () => {
     const username1 = localStorage.getItem('username1')
     const message = localStorage.getItem('message')
 
-    //These are the sources of the innerHTML error we keep getting
-    //They're coming back as null so we need to find a way to think of these
-    if(document.getElementById('amount').innerHTML != null){
-        document.getElementById('amount').innerHTML = amount;
-    }
-    if(document.getElementById('username').innerHTML != null){
-        document.getElementById('username').innerHTML = username;
-    }
-    if(document.getElementById('username1').innerHTML != null){
-        document.getElementById('username1').innerHTML = username1;
-    }
-    if(document.getElementById('message').innerHTML != null){
-        document.getElementById('message').innerHTML = message;
-    }
+
+    document.getElementById('amount').innerHTML = amount;
+    document.getElementById('username').innerHTML = username;
+    document.getElementById('username1').innerHTML = username1;
+    document.getElementById('message').innerHTML = message;
+
 });
 
 
@@ -60,29 +52,18 @@ const Home = () => {
 
 
     return (
-       <body>
-        <><div>
-        
-        </div><div >
-                <h2>What You Can Do</h2>
-                <h5>Create an Account</h5>
-                <h5>Login to an Existing Account</h5>
-                <h5>Make a Transaction</h5>
-                <h5>Look at Public Transactions</h5>
-            </div></><br></br>
-  
-  <div>
-      <h2>Public Transactions</h2>
-      <div> 
-      
+        <><body>
 
-      <h3 class = "box form" ><b> <span id="username1"></span></b> paid <span id="username"></span> $<span id="amount"></span> Message: <span id="message"> </span></h3>
-      </div>
-      
-  </div>
-  <br></br>
-  <br></br>
-            </body> 
+
+            <h2>Public Transactions</h2>
+            <div>
+
+
+                <h3 class="box form"><b> <span id="username1"></span></b> paid <span id="username"></span> $<span id="amount"></span> Message: <span id="message"> </span></h3>
+            </div>
+
+        </body><br></br><br></br></>
+            
     )
 };
 
