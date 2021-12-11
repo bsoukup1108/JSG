cec7534ed2d49bf9ba54524cc316186ea1912646
jzaionz@mail.sfsu.edu <jzaionz@mail.sfsu.edu>
2021-12-11 00:32:39 -0800

    Got rid of a bunch of random commented code that didn't work.

--- back-end/src/main/java/SparkDemo.java
+++ back-end/src/main/java/SparkDemo.java
@@ -66,73 +66,7 @@ public class SparkDemo {
     MongoCollection<Document> myCollection = db.getCollection("Users");
 
     port(1234);
-    //So, in order to actually connect it with a server, we need to utilize the URI or something
-    /*ServerSocket ding;
-    Socket dong = null;
-    try {
-      ding = new ServerSocket(1299);
-      System.out.println("Opened socket " + 1299);
-      while (true) {
-        // keeps listening for new clients, one at a time
-        try {
-          dong = ding.accept(); // waits for client here
-        } catch (IOException e) {
-          System.out.println("Error opening socket");
-          System.exit(1);
-        }
 
-        InputStream stream = dong.getInputStream();
-        BufferedReader in = new BufferedReader(new InputStreamReader(stream));
-        String firstLine = null;
-        try {
-          // read the first line to get the request method, URI and HTTP version
-          String line = in.readLine(); // buffered reader is waiting for an entire line
-          firstLine = line;
-          System.out.println("----------REQUEST START---------");
-          System.out.println(line);
-          // read only headers
-          line = in.readLine();
-          while (line != null && line.trim().length() > 0) {
-            int index = line.indexOf(": ");
-            if (index > 0) {
-              System.out.println(line);
-            } else {
-              break;
-            }
-            line = in.readLine();
-          }
-          System.out.println("----------REQUEST END---------\n\n");
-        } catch (IOException e) {
-          System.out.println("Error reading");
-          System.exit(1);
-        }
-
-        BufferedOutputStream out = new BufferedOutputStream(dong.getOutputStream());
-        PrintWriter writer = new PrintWriter(out, true);  // char output to the client
-
-        // every response will always have the status-line, date, and server name
-        writer.println("HTTP/1.1 200 OK");
-        writer.println("Server: TEST");
-        writer.println("Connection: close");
-        writer.println("Content-type: text/html");
-        writer.println("");
-
-        // Body of our response
-        if(firstLine != null){
-          String url = firstLine.split(" ")[1];
-          writer.println("<h1>Hello, your URL is " + url + "</h1>");
-        }
-        else {
-          writer.println("<h1>Hello World</h1>");
-        }
-
-        dong.close();
-      }
-    } catch (IOException e) {
-      System.out.println("Error opening socket");
-      System.exit(1);
-    }
-    */
     post("/api/sign-up", (req,res) -> {
       String body = req.body();
       System.out.println(body);
